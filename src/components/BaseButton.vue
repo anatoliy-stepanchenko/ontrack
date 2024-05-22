aaa7f1cbec57c7ceae9778247a975c0dd0249fe8
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2024-05-22 18:21:39 +0300

    the stopwatch template

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -21,14 +21,20 @@ defineProps({
 <script>
 import {
   BUTTON_TYPE_DANGER,
+  BUTTON_TYPE_SUCCESS,
+  BUTTON_TYPE_WARNING,
   BUTTON_TYPE_NEUTRAL,
   BUTTON_TYPE_PRIMARY,
 } from "../constants.js"
 
 const typeClassesButton = {
   [BUTTON_TYPE_DANGER]: " bg-red-500 p-3 enabled:hover:bg-red-600 text-white",
+  [BUTTON_TYPE_SUCCESS]:
+    " bg-green-500 p-3 enabled:hover:bg-green-600 text-white",
+  [BUTTON_TYPE_WARNING]:
+    " bg-yellow-400 p-3 enabled:hover:bg-yellow-500 text-white",
   [BUTTON_TYPE_NEUTRAL]: " bg-gray-100 p-3 enabled:hover:bg-gray-200",
-  [BUTTON_TYPE_PRIMARY]: " bg-green-400 p-3 enabled:hover:bg-green-500",
+  [BUTTON_TYPE_PRIMARY]: " bg-blue-400 p-3 enabled:hover:bg-blue-500",
 }
 </script>
 
--- src/constants.js
+++ src/constants.js
@@ -11,11 +11,15 @@ export const PAGE_ACTIVITIES = "activities"
 export const PAGE_PROGRESS = "progress"
 
 export const BUTTON_TYPE_DANGER = "danger"
+export const BUTTON_TYPE_SUCCESS = "success"
+export const BUTTON_TYPE_WARNING = "warning"
 export const BUTTON_TYPE_NEUTRAL = "neutral"
 export const BUTTON_TYPE_PRIMARY = "primary"
 
 export const BUTTON_TYPES = [
   BUTTON_TYPE_DANGER,
+  BUTTON_TYPE_SUCCESS,
+  BUTTON_TYPE_WARNING,
   BUTTON_TYPE_NEUTRAL,
   BUTTON_TYPE_PRIMARY,
 ]
