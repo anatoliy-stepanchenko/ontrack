90527be3fda2912ae8ea152f0f4666c417c94bd6
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2024-05-23 13:29:34 +0300

    start, stop and reset the stopwatch

--- src/components/TimelineStopwatch.vue
+++ src/components/TimelineStopwatch.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex w-full gap-4">
-    <BaseButton :type="BUTTON_TYPE_DANGER"
+    <BaseButton :type="BUTTON_TYPE_DANGER" @click="reset"
       ><ArrowPathIcon class="h-8"
     /></BaseButton>
     <div
@@ -8,33 +8,54 @@
     >
       {{ formatSeconds(seconds) }}
     </div>
-    <BaseButton :type="BUTTON_TYPE_WARNING"
+    <BaseButton v-if="isRunning" :type="BUTTON_TYPE_WARNING" @click="stop"
       ><PauseIcon class="h-8"
     /></BaseButton>
-    <BaseButton :type="BUTTON_TYPE_SUCCESS"
+    <BaseButton v-else :type="BUTTON_TYPE_SUCCESS" @click="start"
       ><PlayIcon class="h-8"
     /></BaseButton>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue"
 import BaseButton from "./BaseButton.vue"
 import { ArrowPathIcon, PauseIcon, PlayIcon } from "@heroicons/vue/24/outline"
 import {
   BUTTON_TYPE_DANGER,
   BUTTON_TYPE_WARNING,
   BUTTON_TYPE_SUCCESS,
+  MILLISECONDS_IN_SECOND,
 } from "../constants"
 import { formatSeconds } from "../functions"
 import { isNumber } from "../validators"
 
-defineProps({
+const props = defineProps({
   seconds: {
     default: 0,
     type: Number,
     validator: isNumber,
   },
 })
+
+const seconds = ref(props.seconds)
+const isRunning = ref(false)
+
+function start() {
+  isRunning.value = setInterval(() => {
+    seconds.value++
+  }, MILLISECONDS_IN_SECOND)
+}
+
+function stop() {
+  clearInterval(isRunning.value)
+  isRunning.value = false
+}
+
+function reset() {
+  stop()
+  seconds.value = 0
+}
 </script>
 
 <style scoped></style>
